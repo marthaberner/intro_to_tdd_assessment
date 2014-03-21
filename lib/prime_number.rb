1d5e347fa099adf81b279b470310bade05f1a7ab
marthaberner <martha@marthaberner.com>
2014-03-21 09:19:36 -0600

    Detects that 3 is a prime number

--- lib/prime_number.rb
+++ lib/prime_number.rb
@@ -4,5 +4,4 @@ class Prime
   def prime_number?(number)
     Prime.prime?(number)
   end
-
 end
--- spec/prime_number_spec.rb
+++ spec/prime_number_spec.rb
@@ -2,9 +2,9 @@ require 'rspec/core'
 require 'prime_number'
 
 describe "PrimeNumber" do
-  it "Detects that 3 is a prime number"
+  it "Detects that 3 is a prime number" do
   new_num = Prime.new
 
- expected = true
-  actual = true
+  expect(true).to eq new_num.prime_number?(3)
+  end
 end
