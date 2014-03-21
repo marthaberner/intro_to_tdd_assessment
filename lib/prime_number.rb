b2f89075e22d8f7acfb9f90a5d1e4632f61f2082
marthaberner <martha@marthaberner.com>
2014-03-21 09:18:00 -0600

    Expects true to equal true, test green

---
+++ lib/prime_number.rb
@@ -0,0 +1,8 @@
+require 'prime'
+
+class Prime
+  def prime_number?(number)
+    Prime.prime?(number)
+  end
+
+end
--- spec/prime_number_spec.rb
+++ spec/prime_number_spec.rb
@@ -1,2 +1,10 @@
 require 'rspec/core'
 require 'prime_number'
+
+describe "PrimeNumber" do
+  it "Detects that 3 is a prime number"
+  new_num = Prime.new
+
+ expected = true
+  actual = true
+end
