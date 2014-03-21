b53bb9a530995917f8bcb5b81d625b165ffcac78
marthaberner <martha@marthaberner.com>
2014-03-21 09:23:21 -0600

    It detects that 101 is a prime number

--- spec/prime_number_spec.rb
+++ spec/prime_number_spec.rb
@@ -13,4 +13,12 @@ describe "PrimeNumber" do
 
     expect(false).to eq new_num.prime_number?(4)
   end
+  it "Detects that 101 is a prime number" do
+    new_num = Prime.new
+
+
+    expect(true).to eq new_num.prime_number?(101)
+  end
+
+
 end
