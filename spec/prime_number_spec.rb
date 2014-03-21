f9ab57ffe9f7d2064c4226f58138b9ebc0cf2f83
marthaberner <martha@marthaberner.com>
2014-03-21 09:20:50 -0600

    Detects that 4 is not a prime number

--- spec/prime_number_spec.rb
+++ spec/prime_number_spec.rb
@@ -7,4 +7,10 @@ describe "PrimeNumber" do
 
   expect(true).to eq new_num.prime_number?(3)
   end
+
+  it "Detects that 4 is not a prime number" do
+    new_num = Prime.new
+
+    expect(false).to eq new_num.prime_number?(4)
+  end
 end
